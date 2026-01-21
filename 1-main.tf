49cf6655dda83a595e0c2d512fb028b6de3a88cd
KeerthiReddyManda <keerthireddymailz@gmail.com>
2026-01-21 16:54:13 -0700

    Update and rename main.tf to 1-main.tf

--- 1-main.tf
+++ 1-main.tf
@@ -1,8 +1,7 @@
 ## Settings block 
 
 terraform {
-    #terraform version
-    required_providers {
+    #terraform version    required_providers {
          google = {
       source  = "hashicorp/google"
       version = "7.16.0"
