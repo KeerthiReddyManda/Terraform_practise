1e48e6ce51f5d2ee44388f8979459cf3a36b703a
KeerthiReddyManda <keerthireddymailz@gmail.com>
2026-01-20 19:53:45 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+## Settings block 
+
+terraform {
+    #terraform version
+    required_providers {
+         google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+    }
+}
+
+## Provider block 
+# Google Provider
+
+provider "google" {
+  project = "var.project_id"
+  region  = "var.region"
+}
