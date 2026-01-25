03661dcaf27f68be25b4cbf80bb1c1e0202d7aec
KeerthiReddyManda <keerthireddymailz@gmail.com>
2026-01-25 16:25:27 -0700

    Update main.tf

--- VM with startup script/main.tf
+++ VM with startup script/main.tf
@@ -1,1 +1,23 @@
+# Settings block 
+
+terraform {
+    #terraform version
+    required_providers {
+         google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+    }
+}
+
+
+# Provider block (Google Provider)
+
+
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone = var.zone
+}
 
