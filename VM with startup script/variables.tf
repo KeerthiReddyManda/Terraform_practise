813fdd4e04f9edf5d604de19164ff8a9e7d8388f
KeerthiReddyManda <keerthireddymailz@gmail.com>
2026-01-25 17:35:26 -0700

    Update variables.tf

--- VM with startup script/variables.tf
+++ VM with startup script/variables.tf
@@ -1,1 +1,19 @@
+variable "project_id" {type = string}
 
+variable "region" {type = string}
+
+variable "zone" {type = string}
+
+variable "vpc_name" {type = string}
+
+variable "subnet_name" {type = string}
+
+variable "cidr_range" {type = string}
+
+variable "vm_name" {type = string}
+variable "machine_type" {type = string}
+variable "image" {type = string}
+
+variable "tags" {
+    type = list(string)
+}
